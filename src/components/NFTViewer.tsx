1929b67c77fa620ad61335ff32ddfd862b7bfa7e
genan566 <avodagbejean@gmail.com>
2023-04-12 04:59:47 +0100

    feat: impression

--- src/components/NFTViewer.tsx
+++ src/components/NFTViewer.tsx
@@ -7,20 +7,41 @@ import { BlurView } from 'expo-blur'
 import { useLoadingFonts } from '../../utilities/LoadingFonts'
 import { Feather, Ionicons } from '@expo/vector-icons';
 import { NftsInterface } from '../types/NFTsInterface'
+import { SaleHistoriesAPI } from '../APIs/SaleHistoriesAPI'
+import { SaleHistory } from '../types/SaleHistoryType'
 const NFTViewer = ({ data }: { data: any, }) => {
     const { height } = useDimensionSizes()
     const { loaded } = useLoadingFonts()
+    const [saleHistories, setSaleHistories] = React.useState<SaleHistory[]>([])
+
+    React.useEffect(() => {
+        load_sale_histories()
+    }, [data?.id])
+
+    const load_sale_histories = async () => {
+        let sales_getted = data?.id
+        if (Boolean(sales_getted)) {
+            let salesHistories_trendings = new SaleHistoriesAPI()
+            salesHistories_trendings
+                .get_multi_sales_by_nftID(sales_getted)
+                .then(datas => {
+                    if (datas.length > 0) {
+                        setSaleHistories([...datas])
+                    }
+                })
+        }
+    }
+
+    console.log(saleHistories.length)
 
     const calculatedSalesAdded = React.useMemo(() => {
-        // if (data.sales_history.length > 0) {
-        //     return data.sales_history.length > 1000 ? `${data.sales_history.length / 1000}K`
-        //         : data.sales_history.length > 1000000 ? `${data.sales_history.length / 1000000}M` : data.sales_history.length
-        // } else {
-        //     return 0
-        // }
-
-        return 0
-    }, [data.sales_history])
+        if (saleHistories.length > 0) {
+            return saleHistories.length > 1000 ? `${saleHistories.length / 1000}K`
+                : saleHistories.length > 1000000 ? `${saleHistories.length / 1000000}M` : saleHistories.length
+        } else {
+            return 0
+        }
+    }, [saleHistories])
 
     return (
         <View style={{
@@ -42,7 +63,7 @@ const NFTViewer = ({ data }: { data: any, }) => {
             }}>
                 <View style={{ position: "absolute", top: 10, left: 10, overflow: "hidden", borderRadius: 100 }}>
                     <BlurView intensity={20} tint="dark" style={{
-                        padding: 10, paddingHorizontal: 20, elevation: 10, display: "flex",
+                        padding: 2.5, paddingHorizontal: 10, elevation: 10, display: "flex",
                         justifyContent: 'space-between', flexDirection: "row", alignItems: "center"
                     }}>
                         <View style={{
@@ -52,7 +73,7 @@ const NFTViewer = ({ data }: { data: any, }) => {
                             <Ionicons name="people-circle" size={24} color="white" />
                             <View>
                                 <Text style={{ color: "rgba(255,255,255,.5)", fontFamily: loaded && "Montserrat-Medium", }}>Followers</Text>
-                                <Text style={{ color: "white", fontFamily: loaded && "Montserrat-SemiBold", fontSize: 20, marginTop: 2.5 }}>{calculatedSalesAdded || 0}K</Text>
+                                <Text style={{ color: "white", fontFamily: loaded && "Montserrat-SemiBold", fontSize: 20, marginTop: 2.5 }}>{calculatedSalesAdded || 0}</Text>
                             </View>
                         </View>
                     </BlurView>
@@ -60,22 +81,22 @@ const NFTViewer = ({ data }: { data: any, }) => {
 
                 <View style={{ position: "absolute", top: 10, right: 10, overflow: "hidden", borderRadius: 100 }}>
 
-                    <View style={{ borderWidth: 1, borderColor: "rgba(255,255,255,.2)", borderRadius: 100,overflow:"hidden" }}>
+                    <View style={{ borderWidth: 1, borderColor: "rgba(255,255,255,.2)", borderRadius: 100, overflow: "hidden" }}>
                         <Image
-                            style={{ width: 45, height: 45,borderRadius: 100 }}
+                            style={{ width: 45, height: 45, borderRadius: 100 }}
                             resizeMode="cover"
                             source={require("../../assets/images/1.png")} />
                     </View>
                 </View>
                 <View style={{ width: "100%", marginBottom: 15 }}>
-                    <Text style={{ fontFamily: loaded && "Montserrat-Medium", fontSize: 30, color: "white",width:"50%" }} numberOfLines={1} >{data.title}</Text>
-                    <Text style={{ fontFamily: loaded && "Montserrat-Medium", fontSize: 25, color: "white",width:"90%" }} numberOfLines={1} >{data.description}</Text>
+                    <Text style={{ fontFamily: loaded && "Montserrat-Medium", fontSize: 30, color: "white", width: "50%" }} numberOfLines={1} >{data.title}</Text>
+                    <Text style={{ fontFamily: loaded && "Montserrat-Medium", fontSize: 25, color: "white", width: "90%" }} numberOfLines={1} >{data.description}</Text>
                 </View>
                 <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", width: "100%" }}>
 
                     <View style={{ overflow: "hidden", backgroundColor: "rgba(82, 82, 79,.8)", borderRadius: 100 }}>
                         <BlurView intensity={20} tint="dark" style={{
-                            padding: 10, paddingHorizontal: 20, elevation: 10, display: "flex",
+                            padding: 8, paddingHorizontal: 20, elevation: 10, display: "flex",
                             justifyContent: 'space-between', flexDirection: "row", alignItems: "center"
                         }}>
                             <View style={{
